ae19bd4119b25df0dc400df361eeaa54e7740f17
Gavin Sun <gavinnicholas0921@gmail.com>
2020-12-11 19:46:24 +0800

    Update comments

--- Demo1.cpp
+++ Demo1.cpp
@@ -1,20 +1,20 @@
 #include "Simulator.h"
 
 int main() {
-    // reset logfile.
+    // Reset logfile.
     Simulator::resetLog();
 
-    // creat a 'Field' named field;
+    // Creat a 'Field' named field.
     Simulator::Field field;
 
-    // put two point into field;
+    // Put two points into field.
     field.addPoint(1, 2, 1); // (x, y, density);
     // field.addPoint(2, 1); // without 'density' parameter, it will go addPoint(x, y, 0);
 
-    // set centre;
+    // Set centre;
     field.setCentre(0, 0); // (x, y);
 
-    // get radiation count with 'r' = 1.
+    // Get radiation count with 'r' = 1.
     field.getRadiationCount(1);
 
     return 0;
